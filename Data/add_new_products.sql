6d64cc012ee54f0436d091082d4e7cc2f0528c26
JakeWin97 <Jake97Winwood@gmail.com>
2021-09-17 15:30:07 +0000

    Add more products

--- Data/add_new_products.sql
+++ Data/add_new_products.sql
@@ -8,7 +8,6 @@ INSERT INTO CodeAcademy.product (product_name,selling_price,category,img) VALUES
      ('Carrots', 0.5, 1, '/images/products/fruitveg/carrots.jpg'),
      ('Peas', 0.7, 1, '/images/products/fruitveg/peas.jpg'),
 
-
      ('Butter', 1.3, 2, '/images/products/dairy/butter.jpg'),
      ('Cheddar Cheese', 2, 2, '/images/products/dairy/cheddar.jpg'),
      ('Eggs', 0.9, 2, '/images/products/dairy/eggs.jpg'),
@@ -22,10 +21,20 @@ INSERT INTO CodeAcademy.product (product_name,selling_price,category,img) VALUES
      ('Bacon', 1.25, 3, '/images/products/meat/bacon.jpg'),
      ('Sausages', 1.50, 3, '/images/products/meat/sausages.jpg'),
 
-     ('Corn Flakes', 2, 4, '/images/products/cereal/corn_flakes.jpg'),
+     ('Corn Flakes', 2, 4, '/images/products/breakfast/corn_flakes.jpg'),
+     ('Tea Bags', 3, 4, '/images/products/breakfast/tea_bags.jpg'),
+     ('Coffee', 2, 4, '/images/products/breakfast/coffee.jpg'),
+     ('Shreddies', 2, 4, '/images/products/breakfast/shreddies.jpg'),
+     ('Sugar Puffs', 2, 4, '/images/products/breakfast/sugar_puffs.jpg'),
 
      ('Toilet Roll', 2.50, 5, '/images/products/bathroom/toilet_roll.jpg'),
      ('Toothpaste', 1.25, 5, '/images/products/bathroom/toothpaste.jpg'),
      ('Mouthwash', 1.20, 5, '/images/products/bathroom/mouthwash.jpg'),
      ('Soap', 0.98, 5, '/images/products/bathroom/soap.jpg'),
-     ('Shampoo', 2, 5, '/images/products/bathroom/shampoo.jpg');
+     ('Shampoo', 2, 5, '/images/products/bathroom/shampoo.jpg'),
+
+     ('Pepperoni Pizza', 1.20, 6, '/images/products/frozen/pizza.jpg'),
+     ('Oven Chips', 2, 6, '/images/products/frozen/chips.jpg'),
+     ('Chicken Dippers', 1.55, 6, '/images/products/frozen/dippers.jpg'),
+     ('Vanilla Ice Cream', 2, 6, '/images/products/frozen/icecream.jpeg'),
+     ('Potato Waffles', 1.25, 6, '/images/products/frozen/waffles.jpg');
